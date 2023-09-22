01e4ae1b5a6f12e2de7354f147d2dfa4d4300198
Sasikala S <102387885+Sksanth@users.noreply.github.com>
2023-09-22 12:06:21 -0400

    #11 install AWS terraform provider, create and destro S3 bucket (#12)

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
   }
 }
 
@@ -11,10 +15,24 @@ provider "random" {
   # Configuration options
 }
 
-resource "random_string" "bucket_name" {
-  length   = 16
-  special  = false
+provider "aws" {
 }
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "bucket_name" {
+  length = 32
+  lower = true
+  upper = false
+  special = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = random_string.bucket_name.result
+}
+
 
 output "random_bucket_name" {
   value = random_string.bucket_name.result
