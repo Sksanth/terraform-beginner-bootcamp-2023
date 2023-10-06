b382e61aa21cbfb249130a8ce8bc0ffc9ecb8f13
Sasi S <sasthumne@gmail.com>
2023-10-06 15:02:25 +0000

    #44 test our terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -16,32 +16,31 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid 
+  token = var.terratowns_access_token
 }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = "${path.root}${var.index_html_filepath}"
-#   error_html_filepath = "${path.root}${var.error_html_filepath}"
-#   content_version = var.content_version
-#   assets_path = "${path.root}${var.assets_path}" 
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  bucket_name = var.bucket_name
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = "${path.root}${var.index_html_filepath}"
+  error_html_filepath = "${path.root}${var.error_html_filepath}"
+  content_version = var.content_version
+  assets_path = "${path.root}${var.assets_path}" 
+}
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!"
+  name = "October 2023"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+Autumn is generally regarded as the end of the growing season. 
+In the autumn season, the daylight grows shorter, and animals prepare for the long, cold months ahead. 
+The temperature starts becoming cooler during autumn. 
+Leaves on the trees will turn yellow, orange, red and brown during autumn.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
   content_version = 1
 }
 
