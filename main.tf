f214e4c8e3b736e69aec4f2ba4f184aec76e3b17
Sasi S <sasthumne@gmail.com>
2023-10-14 20:13:12 +0000

    #52 remove duplicate module

--- main.tf
+++ main.tf
@@ -21,45 +21,6 @@ provider "terratowns" {
   token = var.terratowns_access_token
 }
 
-/*
-module "home_fall_hosting" {
-  source = "./modules/terrahome_aws"
-  user_uuid = var.teacherseat_user_uuid
-  public_path = var.fall.public_path
-  content_version = var.fall.content_version
-}
-
-resource "terratowns_home" "home" {
-  name = "October 2023"
-  description = <<DESCRIPTION
-Autumn is generally regarded as the end of the growing season. 
-In the autumn season, the daylight grows shorter, and animals prepare for the long, cold months ahead. 
-The temperature starts becoming cooler during autumn. 
-Leaves on the trees will turn yellow, orange, red and brown during autumn.
-DESCRIPTION
-  domain_name = module.home_fall_hosting.domain_name
-  town = "missingo"
-  content_version = var.fall.content_version
-}
-*/
-
-module "home_kpop_hosting" {
-  source = "./modules/terrahome_aws"
-  user_uuid = var.teacherseat_user_uuid
-  public_path = var.kpop.public_path
-  content_version = var.kpop.content_version
-}
-
-resource "terratowns_home" "home_kpop" {
-  name = "K-Pop"
-  description = <<DESCRIPTION
-Korean popular music, is a form of popular music originating in South Korea as part of South Korean culture.
-DESCRIPTION
-  domain_name = module.home_kpop_hosting.domain_name
-  town = "melomaniac-mansion"
-  content_version = var.kpop.content_version
-}
-
 module "home_kpop_hosting" {
   source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
